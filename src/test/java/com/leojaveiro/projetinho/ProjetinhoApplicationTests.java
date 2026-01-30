50e439d153e5e916824e7379847676c73190ba9e
Leo <dequeijo532@gmail.com>
2026-01-30 16:48:08 -0300

    Project created

---
+++ src/test/java/com/leojaveiro/projetinho/ProjetinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leojaveiro.projetinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
