e57ff8b3a89cc49bb5c4fc48dcd3c4d162578fec
Leo <dequeijo532@gmail.com>
2026-02-03 18:39:38 -0300

    Product

---
+++ src/main/java/com/leojaveiro/projetinho/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.leojaveiro.projetinho.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leojaveiro.projetinho.entities.Product;
+import com.leojaveiro.projetinho.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
